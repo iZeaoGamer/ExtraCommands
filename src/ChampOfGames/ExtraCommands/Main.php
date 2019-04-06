2b6917d735d383650980ed2afc899a813b912e56
ChampOfGamesYT <40720714+ChampOfGamesYT@users.noreply.github.com>
2019-04-06 12:21:51 +0300

    Added /day /night

--- src/ChampOfGames/ExtraCommands/Main.php
+++ src/ChampOfGames/ExtraCommands/Main.php
@@ -92,7 +92,31 @@ class Main extends PluginBase {
             }
           return true;
       }
-      
+		
+      if($cmd->getName() == "day") {
+   	if($sender instanceof Player) {
+   	 if($sender->hasPermission("day.use")) {
+   	    $sender->getLevel()->setTime(6000);
+            $sender->sendMessage($this->fts . TF::GREEN . " You have set the time to day!");
+            }else{
+            $sender->sendMessage($this->fts . TF::RED . "You are not allowed to use this command");
+               }
+            }
+          return true;
+      }
+		
+	if($cmd->getName() == "night") {
+   	if($sender instanceof Player) {
+   	 if($sender->hasPermission("night.use")) {
+   	    $sender->getLevel()->setTime(16000);
+            $sender->sendMessage($this->fts . TF::GREEN . " You have set the time to night!");
+            }else{
+            $sender->sendMessage($this->fts . TF::RED . "You are not allowed to use this command");
+               }
+            }
+          return true;
+      }	
+		
 	if($cmd->getName() == "fly") {
 	  if($sender instanceof Player) {
 		if($sender->hasPermission("fly.use")) {
